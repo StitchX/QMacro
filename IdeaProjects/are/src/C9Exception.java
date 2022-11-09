35cb4cab470953c8cfab4aba81caab51d7ec9a54
StitchX <1916291530@qq.com>
2022-11-09 11:43:40 +0800

    canner

--- IdeaProjects/are/src/C9Exception.java
+++ IdeaProjects/are/src/C9Exception.java
@@ -1,2 +1,21 @@
+import java.io.IOException;
+
 public class C9Exception {
+
+    public void aa() throws InterruptedException {
+        Thread.sleep(1000*3);
+    }
+
+    public static void main(String[] args) {
+        try {
+
+        }catch (ArrayIndexOutOfBoundsException a){
+
+        }catch (IndexOutOfBoundsException i){
+
+        }finally {
+            System.out.println("无论任何方式都会输出；非强制写入");
+        }
+
+    }
 }
